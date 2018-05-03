3bee9e5252c08a63c585d68ddd8e817d195117eb
alexshakouri <32425085+alexshakouri@users.noreply.github.com>
2018-05-02 23:27:00 -0700

    Update writeSubmission.py

--- Submission/writeSubmission.py
+++ Submission/writeSubmission.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed May  2 18:19:20 2018
-
-@author: chama
+@author: CHAMAIN AND ALEX (mostly ALEX ;D)
 """
 
 import csv
 import numpy as np
-y_Test = np.load('D:/PrivacyPreservingDistributedDL/Filters/y_Test.npy')
+import pandas as pd
+y_Test = np.load('y_Test.npy')
 print('y_Test loaded with size',y_Test.shape)
-#print(y_Test[66,:])
+print(y_Test[1,:])
 
+imageId = np.arange(1, y_Test.shape[0]+1)
 
-with open('submission.csv', 'w') as csvfile:
-    fieldnames = ['image_id', 'label_id']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+labelId = np.zeros(y_Test.shape[0], dtype=object)
 
-    writer.writeheader()
-    for i in range(0,y_Test.shape[0]-1):
-        label_indices = np.asarray(np.where(y_Test[i,:]>0))
-        print(label_indices)
-        label_str = np.array2string(label_indices,separator=' ')
-        writer.writerow({'image_id': '{}'.format(i+1), 'label_id':label_str })
+for i in range(0, y_Test.shape[0]):
+    label_indices = np.array(np.where(y_Test[i,:]>0))
+    
+    #This joins together all the indices that we marked correctly
+    labelId[i] = ''.join(str(label_indices))
+    #Gets rid of the extra [] that gets put into the string
+    labelId[i] = labelId[i].strip('[]')
+    #print(labelId[i])
+
+print (labelId)
+
+#saves the two types of data into columns in the file
+submission = pd.DataFrame({'image_id' : imageId, 'label_id' : labelId})
+submission.to_csv("submission.csv", index=False)
+
