bbf8e22717d1de0a8545d3682e905b949900360d
chamain <chamain999@gmail.com>
2018-05-31 17:46:45 -0700

    Add files via upload

--- fashion_CNN/recent/writeSubmission.py
+++ fashion_CNN/recent/writeSubmission.py
@@ -7,25 +7,28 @@ Created on Wed May  2 18:19:20 2018
 import csv
 import numpy as np
 import pandas as pd
-y_Test = np.load('D:\PrivacyPreservingDistributedDL\Filters\y_TestSub_64.npy')
-#print('y_Test loaded with size',y_Test.shape)
-#print(y_Test[1,:])
+import datetime
 
-imageId = np.arange(1, y_Test.shape[0]+1)
-
-labelId = np.zeros(y_Test.shape[0], dtype=object)
-
-for i in range(0, y_Test.shape[0]):
-    label_indices = np.array(np.where(y_Test[i,:]>0))
+def writeSub(Y_directory):
+    y_Test=Y_directory
+    #print('y_Test loaded with size',y_Test.shape)
+    #print(y_Test[1,:])
     
-    #This joins together all the indices that we marked correctly
-    labelId[i] = ''.join(str(label_indices))
-    #Gets rid of the extra [] that gets put into the string
-    labelId[i] = labelId[i].strip('[]')
-    #print(labelId[i])
-
-print (labelId)
-
-#saves the two types of data into columns in the file
-submission = pd.DataFrame({'image_id' : imageId, 'label_id' : labelId})
-submission.to_csv("submission.csv", index=False)
+    imageId = np.arange(1, y_Test.shape[0]+1)
+    
+    labelId = np.zeros(y_Test.shape[0], dtype=object)
+    
+    for i in range(0, y_Test.shape[0]):
+        label_indices = np.array(np.where(y_Test[i,:]>0))+1
+        
+        #This joins together all the indices that we marked correctly
+        labelId[i] = ''.join(str(label_indices))
+        #Gets rid of the extra [] that gets put into the string
+        labelId[i] = labelId[i].strip('[]')
+        #print(labelId[i])
+    
+    print (labelId)
+    
+    #saves the two types of data into columns in the file
+    submission = pd.DataFrame({'image_id' : imageId, 'label_id' : labelId})
+    submission.to_csv("D:\PrivacyPreservingDistributedDL\Filters\Submissions\submission{}.csv".format(datetime.datetime.now().strftime("%y-%m-%d-%H-%M")), index=False)
